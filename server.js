1f3c60835e40071be8d296c8b0ffb5b1b79f1f4a
Adassuriyage Don Leelaka Indira <liad@deakin.edu.au>
2021-05-29 19:40:58 +1000

    testing

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const req = require('request');
 const ejs = require('ejs');
 const passport = require('passport');
 const mongoose = require('mongoose');
-const session = require('express-session');
+//const session = require('express-session');
 const {ensureAuthenticated, forwardAuthenticated} = require('./ensureAuth');
 
 //require('../spsf_service/googleauth');
@@ -23,29 +23,29 @@ var loggedIn=false;
 var loggedUsername ='';
 
 //passport google 
-require('./googleauth');
+// require('./googleauth');
 
-try {
+// try {
     // Connect to the MongoDB cluster
-    mongoose.connect(
-    uri,{ useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true},
-        () => console.log(" Mongoose is connected...")
-    ); } catch (e) {
-    console.log("could not connect...");
-  }
+//     mongoose.connect(
+//     uri,{ useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true},
+//         () => console.log(" Mongoose is connected...")
+//     ); } catch (e) {
+//     console.log("could not connect...");
+//   }
 
 
 //express session
-app.use(session({
-    secret: "key to cookie",
-    resave: true,
-    saveUninitialized: true,
-  })
-);
+// app.use(session({
+//     secret: "key to cookie",
+//     resave: true,
+//     saveUninitialized: true,
+//   })
+// );
 
 // Passport middleware
-app.use(passport.initialize());
-app.use(passport.session());
+// app.use(passport.initialize());
+// app.use(passport.session());
 
 //spsf index page to sign in (landing or the signin page)
 app.get('/', (request,response) => {
@@ -217,17 +217,17 @@ app.get('/getAllAvailableParkingData', function (request,response){
     })          
 })
 
-app.get('/google', (passport.authenticate('google', {scope: 'profile'})));
+// app.get('/google', (passport.authenticate('google', {scope: 'profile'})));
 
-app.get('/google/callback', function(request, response) {
-    authed= req.authed;
-    if(authed){
-        response.render('displayDashboard', {title: 'SPSF - Dashboard',username:loggedUsername,loggedIn:loggedIn, signIn:false});
+// app.get('/google/callback', function(request, response) {
+//     authed= req.authed;
+//     if(authed){
+//         response.render('displayDashboard', {title: 'SPSF - Dashboard',username:loggedUsername,loggedIn:loggedIn, signIn:false});
 
-    }else{
-        response.render('index', {title: 'SPSF - Home', username:loggedUsername,password:'',message:'',loggedIn:loggedIn, signIn:false});
-    }    
-});
+//     }else{
+//         response.render('index', {title: 'SPSF - Home', username:loggedUsername,password:'',message:'',loggedIn:loggedIn, signIn:false});
+//     }    
+// });
 
 app.listen(port);
 console.log('Server listening on : '+port);
