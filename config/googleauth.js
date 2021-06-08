c5b31070e15d8d1af76b7dbd476a18ea17d56b82
Leelaka Adassuriya <37642488+Leelaka@users.noreply.github.com>
2021-06-08 14:03:05 +1000

    Update googleauth.js
    change callback uri google

--- config/googleauth.js
+++ config/googleauth.js
@@ -7,7 +7,8 @@ const GoogleStrategy = require('passport-google-oauth20').Strategy;
 passport.use(new GoogleStrategy({
     clientID: '453724700226-0h2fj3ijlmkkbc3inl37mdpi7vbc2qdn.apps.googleusercontent.com',
     clientSecret: 'swrlV8tlVbyLzZj1F_619E8k',
-    callbackURL: "http://localhost:3000/google/callback",
+    //callbackURL: "http://localhost:3000/google/callback",
+    callbackURL: "http://spsfwebfront.mybluemix.net/google/callback",
     passReqToCallback: true
   },
   function(request, accessToken, refreshToken, profile, done) {
